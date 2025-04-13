c2adcc3637190fdc4f0ca78c85e71a4a540afd41
Sitti Nur Aisya <sittinuraisya1104@gmail.com>
2025-04-13 23:47:40 +0800

    E-Cuti

--- tes_webcheckin/frontend/src/components/QRGenerator.js
+++
@@ -1,9 +0,0 @@
-import QRCode from "qrcode.react";
-
-function QRGenerator({ eventId }) {
-  return (
-    <div>
-      <QRCode value={`https://yourapp.com/checkin/${eventId}`} />
-    </div>
-  );
-}
--- tes_webcheckin/frontend/src/pages/Admin/AdminPage.js
+++
@@ -1,4 +0,0 @@
-import React from "react";
-export default function AdminPage() {
-  return <h1>Halaman Admin</h1>;
-}
