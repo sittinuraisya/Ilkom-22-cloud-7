d4d55a42051d2daa74803f3b7e5ed9217f9fa543
Sitti Nur Aisya <sittinuraisya1104@gmail.com>
2025-04-14 19:51:31 +0800

    web testing e-cuti

--- ppl_e-cuti/forms/admin_forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, DateField, SubmitField, BooleanField
-from wtforms.validators import DataRequired
-
-class HolidayForm(FlaskForm):
-    name = StringField('Nama Hari Libur', validators=[DataRequired()])
-    date = DateField('Tanggal', validators=[DataRequired()], format='%Y-%m-%d')
-    is_recurring = BooleanField('Berulang Setiap Tahun')
-    submit = SubmitField('Simpan')
--- tes_webcheckin/backend/models/Attendee.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const attendeeSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  eventId: String,
-});
-
-module.exports = mongoose.model("Attendee", attendeeSchema);
