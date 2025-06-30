60427a0d30d36e85d549b70c772b8263733cc5b3
F1G122049 <rahmawatifitrah85@gmail.com>
2025-06-30 13:38:02 +0800

    4

---
+++ Fitrah/data.py
@@ -0,0 +1,36 @@
+[
+  {
+    "nama": "Nasi Goreng",
+    "bahan": [
+      "2 piring nasi putih",
+      "2 siung bawang putih",
+      "1 butir telur",
+      "Kecap manis secukupnya",
+      "Garam dan merica"
+    ],
+    "langkah": [
+      "Tumis bawang putih hingga harum.",
+      "Masukkan telur, orak-arik.",
+      "Masukkan nasi, aduk rata.",
+      "Tambahkan kecap, garam, dan merica.",
+      "Aduk hingga matang. Sajikan hangat."
+    ]
+  },
+  {
+    "nama": "Mie Goreng",
+    "bahan": [
+      "1 bungkus mie instan",
+      "Sayuran (kol, wortel)",
+      "2 siung bawang putih",
+      "1 butir telur",
+      "Kecap manis dan saus sambal"
+    ],
+    "langkah": [
+      "Rebus mie hingga matang, tiriskan.",
+      "Tumis bawang putih, lalu telur.",
+      "Masukkan sayuran, aduk rata.",
+      "Masukkan mie dan bumbu.",
+      "Aduk hingga matang, sajikan."
+    ]
+  }
+]
