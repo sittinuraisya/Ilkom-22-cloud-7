b4fda765ca07ccdb01ae1d7556657d43e180ecf8
F1G122049 <rahmawatifitrah85@gmail.com>
2025-06-30 13:15:36 +0800

    fit

---
+++ Fitrah/app.py
@@ -0,0 +1,31 @@
+# app.py
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+
+app = Flask(__name__)
+
+tasks = []
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify(tasks)
+
+@app.route('/add', methods=['POST'])
+def add_task():
+    task = request.form.get('task')
+    if task:
+        tasks.append(task)
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:index>', methods=['POST'])
+def delete_task(index):
+    if 0 <= index < len(tasks):
+        tasks.pop(index)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
